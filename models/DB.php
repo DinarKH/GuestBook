4c73f98b8ade2c743c0bfbfa956856c0c61dca0f
DinarKH <dinarkhalimov@gmail.com>
2018-07-24 11:48:13 +0400

    dbchange

--- models/DB.php
+++ models/DB.php
@@ -11,3 +11,13 @@ function connect(){
 	return $dbh;
 }
 
+function connect2(){
+	$params=array(
+				'host'=>'http://dev1.msoft.su',
+				'dbname'=>'Guest',
+				'user' => 'learningtest3',
+				'pass' => 'm6sCwXLi',
+			);
+	$dbh=new PDO('mysql:host='.$params['host'].'; dbname='.$params['dbname'].'; charset=utf8',$params['user'],$params['pass']);
+	return $dbh;
+}
